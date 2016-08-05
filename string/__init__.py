60660415daee4f693da7f51959b43dcfc8867f21
zhou.jack <hkebao@126.com>
2016-08-05 22:43:24 +0800

    第一章节 对字符串进行处理

---
+++ string/__init__.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8 -*-
+__author__ = 'yaofang.zjl'
+__created_date__ = '2016/8/5'
+
+"""(Please descripbe what is it briefly.)
+
+     (If it is just a lib or a part of one tool, please delete message below.)
+     (If it is a standalone tool, please add some common command lines below.)
+     Usage:
+"""
